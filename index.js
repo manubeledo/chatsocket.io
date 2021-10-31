e7d7773bd8a58eeac210b8ffa0794dc0fc782157
Your Name <you@example.com>
2021-10-31 18:21:30 -0300

    first commit

--- index.js
+++ index.js
@@ -1,57 +1,80 @@
-const express = require('express');
-const app = express();
+const fs = require("fs");
+const express = require("express");
+let { Server: HttpServer } = require("http");
+let { Server: IOServer } = require("socket.io");
 const handlebars = require("express-handlebars");
-const cors = require('cors')
-// const serverRoutes = require("./routes");
-const path = require("path")
+const cors = require("cors");
+const path = require("path");
 const PORT = 8080;
-const {productos} = require('./productos')
+const { productos } = require("./productos");
 
-app.use("/", express.static(path.join(__dirname,"public")));
+// Initializations
+const app = express();
+const httpServer = new HttpServer(app);
+const io = new IOServer(httpServer);
+
+// Configurations
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
-app.engine("hbs", 
-handlebars({
-        extname: ".hbs",
-        defaultLayout: "index.hbs",
-        layoutsDir: __dirname + "/views/layouts",
-        partialsDir: __dirname + "/views/partials/"
-    })
+// Set hbs engine
+
+app.engine(
+  "hbs",
+  handlebars({
+    extname: ".hbs",
+    defaultLayout: "index.hbs",
+    layoutsDir: __dirname + "/views/layouts",
+    partialsDir: __dirname + "/views/partials/",
+  })
 );
-
 app.set("view engine", "hbs");
 app.set("views", "./views/layouts");
-// app.use(express.static("public"));
 
-// serverRoutes(app);
+app.post("/productos", (req, res, next) => {
+  console.log("le pega a post productos");
+  // res.redirect('/productos');
+});
 
+app.get("/productos", (req, res, next) => {
+  console.log("le pega a get productos");
+  res.render("index", notes);
+});
 
-app.post("/productos", (req, res, next)=>{
-    productos.push(req.body);
-    res.redirect('/')
-})
+httpServer.listen(PORT, () => {
+  console.log(`Desafio funcionando en la URL http://localhost:${PORT}/productos`);
+});
 
-app.get("/productos", (req, res, next)=>{
-    console.log(productos);
-    res.render('index', productos);
-})
+let notes = [];
+let msgs = [];
 
-const server = app.listen(PORT, () => {
-    console.log(`Servidor funcionando en la URL http://localhost:${PORT}`);
-})
+io.on("connection", (socket) => {
+  console.log("new connection", socket.id);
+  io.sockets.emit("server:loadnotes", notes);
+  io.sockets.emit("server:loadmessages", msgs);
 
+  socket.on("client:newnote", (data) => {
+    console.log(data);
+    const note = {
+      title: data.title,
+      description: data.price,
+      thumbnail: data.thumbnail,
+    };
+    notes.push(note);
+    console.log(notes);
+    io.sockets.emit("server:newnote", note);
+  });
 
+  socket.on('client:newmessage', (data) => {
+      console.log(data);
+      const msg = {
+          mail: data.mail,
+          message: data.message,
+          time: data.time
+      }
+      msgs.push(msg);
+      io.sockets.emit("server:newmessage", msg);
+  })
 
-
-
-
-
-
-
-
-
-
-
-
+});
